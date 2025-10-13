5f89f48322f6687c3ccfe4dfcd3affb6a703edc7
Zack <168221270+Zval321@users.noreply.github.com>
2025-10-12 20:11:40 -0500

    Fix CORS for deployed frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
+# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173', 'https://f-f-frontend-kjo44jv6e-zackvalavanis-projects.vercel.app'
+    origins 'http://localhost:5173', 
+            'https://f-f-frontend-kjo44jv6e-zackvalavanis-projects.vercel.app',
+            'https://f-f-frontend.vercel.app'   # add this one too
 
     resource '*',
       headers: :any,
