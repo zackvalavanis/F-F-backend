0a5f92dfb80adec974446bd0cb66e5e68a8dab9f
Zack <168221270+Zval321@users.noreply.github.com>
2025-10-26 19:45:29 -0500

    update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,11 +3,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:5173', 
             'https://f-f-frontend-kjo44jv6e-zackvalavanis-projects.vercel.app',
-            'https://f-f-frontend.vercel.app'   # add this one too
+            'https://f-f-frontend.vercel.app'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: false
+      credentials: true
   end
 end
